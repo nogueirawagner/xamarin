2a4b9ecdd190d6757564865c1e00c3b8e523e20e
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-10-28 08:44:26 -0200

    Configurando material design para o xamarin.forms

--- App/App.Android/MainActivity.cs
+++ App/App.Android/MainActivity.cs
@@ -1,16 +1,13 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace App.Droid
 {
-  [Activity(Label = "App", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-  public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+  [Activity(Label = "App", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  public class MainActivity : FormsAppCompatActivity
   {
     protected override void OnCreate(Bundle bundle)
     {
@@ -19,7 +16,7 @@ namespace App.Droid
 
       base.OnCreate(bundle);
 
-      global::Xamarin.Forms.Forms.Init(this, bundle);
+      Forms.Init(this, bundle);
       LoadApplication(new App());
     }
   }
