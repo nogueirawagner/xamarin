c337920cd483d727ccf8948f5ff3379c09199c7b
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-10-28 09:57:47 -0200

    Criando camada de acesso a dados no android

---
+++ App/App.Android/Database/XAndroidDB.cs
@@ -0,0 +1,23 @@
+﻿
+using App.Droid.Database;
+using App.MapeamentoRelacional;
+using SQLite;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(XAndroidDB))]
+namespace App.Droid.Database
+{
+  public class XAndroidDB : XIDatabase
+  {
+    public SQLiteConnection GetConnection()
+    {
+      var nomedb = "app.db3";
+      var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+      var path = Path.Combine(folder, nomedb);
+
+      return new SQLiteConnection(path);
+    }
+  }
+}
